416e2cdcf6168e2c1882bee796a158c27bd6d18d
fedorchaklm <fedorchak.luba@gmail.com>
2025-01-14 14:32:30 +0200

    hw3/task3 with useNavigate

--- src/components/user-cart/UserCart.tsx
+++ src/components/user-cart/UserCart.tsx
@@ -1,4 +1,4 @@
-import {Link, useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import {ICart} from "../../model/ICart.ts";
 import './UserCart.css';
@@ -9,6 +9,11 @@ import {getTotalPriceOfCarts} from "../../utils/utils.ts";
 export const UserCart = () => {
     const [carts, setCarts] = useState<Array<ICart>>([]);
     const {id} = useParams();
+    const navigate = useNavigate();
+
+    const handleOnClickNavigate = () => {
+        navigate('/users');
+    }
 
     useEffect(() => {
         if (id) {
@@ -25,7 +30,7 @@ export const UserCart = () => {
             {carts.length > 0 ? <p>Cart:</p> : <p>Empty cart</p>}
             {carts.map((cart) => <CartsList key={cart.id} cart={cart}/>)}
             {carts.length > 1 ? <p className='text-2xl'>Total price of carts: {getTotalPriceOfCarts(carts)}$</p>: <p></p>}
-            <Link className='border-2 px-1 bg-blue-50 hover:bg-blue-200 mt-2' to={'/users'}>Back</Link>
+            <button className='border-2 px-1 bg-blue-50 hover:bg-blue-200 mt-2' onClick={handleOnClickNavigate}>Back</button>
         </div>
     )
 }
--- src/components/user-component/UserComponent.tsx
+++ src/components/user-component/UserComponent.tsx
@@ -1,16 +1,22 @@
 import {FC} from "react";
 import IUser from "../../model/IUser.ts";
-import {Link} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 type UserPropsType = {
     user: IUser;
 }
 
 export const UserComponent: FC<UserPropsType> = ({user}) => {
+    const navigate = useNavigate();
+
+    const handleOnCliclNavigate = () => {
+        navigate(`/users/${user.id}/carts`);
+    }
+
     return (
         <div className='px-2 mb-2 flex flex-row items-center gap-2'>
             <p>{user.id} {user.firstName} {user.lastName}</p>
-        <Link className='border-2 px-1 bg-blue-50 hover:bg-blue-200' to={`/users/${user.id}/carts`}>Go to cart</Link>
+        <button className='border-2 px-1 bg-blue-50 hover:bg-blue-200' onClick={handleOnCliclNavigate}>Go to cart</button>
         </div>
     )
 }
