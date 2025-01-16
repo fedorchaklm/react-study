aa1626e92594236d595ffe104941580a2d03c506
fedorchaklm <fedorchak.luba@gmail.com>
2025-01-16 17:29:08 +0200

    hw4

--- src/services/user.api.service.tsx
+++ src/services/user.api.service.tsx
@@ -7,7 +7,11 @@ const baseUrl = import.meta.env.VITE_API_URL;
 export const userApiService = {
     getUsersByPage: async (page: string): Promise<IResponseModel> => {
         const skip = (limitOfUsersByPage * Number(page) - limitOfUsersByPage).toString();
-        const url = getUrlWithSearchParam(baseUrl, 'skip', skip);
+        const searchParams = {
+            limit: limitOfUsersByPage.toString(),
+            skip: skip
+        };
+        const url = getUrlWithSearchParam(baseUrl, searchParams);
         const response = await fetch(url);
         return await response.json();
     }
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,9 +1,15 @@
-export const getUrlWithSearchParam = (requestUrl: string, searchParam: string, searchParamValue:string): string => {
+interface ISearchParams {
+    [key: string]: string;
+}
+
+export const getUrlWithSearchParam = (requestUrl: string, searchParam: ISearchParams): string => {
     const url = new URL(requestUrl);
-    url.searchParams.append( searchParam, searchParamValue);
+    for (const key in searchParam) {
+        url.searchParams.append(key, searchParam[key]);
+    }
     return url.toString();
 }
 
 export const getQuantityOfPages = (total: number, limit: number): number => {
-    return Math.round(total / limit);
+    return Math.ceil(total / limit);
 }
