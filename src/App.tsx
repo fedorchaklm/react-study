8bc94d84ec8d539b482de91a544c0df44507fe8b
fedorchaklm <fedorchak.luba@gmail.com>
2025-01-17 14:36:46 +0200

    lesson 4-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css'
+import {Form1} from "./components/form1/Form1.tsx";
 
 function App() {
 
     return (
-     <></>
+     <>
+         <Form1/>
+     </>
     )
 }
 
---
+++ src/components/form1/Form1.tsx
@@ -0,0 +1,47 @@
+import {FC, FormEvent, useState} from "react";
+
+interface IFormStateProps {
+    username: string;
+    password: string;
+}
+
+export const Form1: FC = () => {
+    const [formState, setFormState] = useState<IFormStateProps>({
+        username: '',
+        password: ''
+    });
+
+    const handleOnSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        const data = {
+            username: formState.username,
+            password: formState.password
+        };
+        console.log(data);
+    }
+
+    // const handleUsernameChange = (e: FormEvent) => {
+    //     const input = e.target as HTMLInputElement;
+    //     setFormState({...formState, username: input.value});
+    // }
+
+    // const handlePasswordChange = (e: FormEvent) => {
+    //     const input = e.target as HTMLInputElement;
+    //     setFormState({...formState, password: input.value});
+    // }
+
+    const handleInputChange = (e: FormEvent) => {
+        const input = e.target as HTMLInputElement;
+        setFormState({...formState, [input.name]: input.value});
+    }
+
+    return (
+        <form onSubmit={handleOnSubmit} className='flex justify-center gap-4 mt-2'>
+            <input className='border-2' type='text' name='username' value={formState.username}
+                   onChange={handleInputChange}/>
+            <input className='border-2' type='text' name='password' value={formState.password}
+                   onChange={handleInputChange}/>
+            <button className='border-2 px-2 bg-black text-white' type='submit'>Click</button>
+        </form>
+    )
+}
